55f3f7b930b799af844686c5c425bc30fde931da
varunlohade <varunlohade@gmail.com>
2023-11-07 13:14:31 +0530

    adding env file

---
+++ ot.js
@@ -0,0 +1,37 @@
+const { configDotenv } = require('dotenv');
+const Telegrambot = require('node-telegram-bot-api');
+
+require("dotenv").config();
+
+console.log(process.env.TELEGRAM_API_KEY);
+
+const bot = new Telegrambot(process.env.TELEGRAM_API_KEY, {
+    polling: true
+});
+
+if (bot.isPolling){
+    console.log('bot is polling');
+}
+
+bot.on('message', function(msg){
+ console.log('Received a message');
+
+ const res = `Hi ${msg.from.first_name}! I received your message: ${msg.text}`;
+
+ bot.sendMessage(msg.chat.id, res);
+})
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
